4fb34fcd868ff285364b594d47f26512ea2ad236
Bram Rongen <bram@thx.network>
2022-02-24 10:58:07 +0100

    Remove duplicate email checking from update since we're not allowing the email to be updated anyway

--- src/controllers/account/patch.action.ts
+++ src/controllers/account/patch.action.ts
@@ -1,26 +1,19 @@
 import { Request, Response } from 'express';
 import { AccountService } from '../../services/AccountService';
-import { NotFoundError, UnprocessableEntityError } from '../../util/errors';
+import { NotFoundError } from '../../util/errors';
 
 export const patchAccount = async (req: Request, res: Response) => {
     const account = await AccountService.get(req.params.id);
     if (!account) {
         throw new NotFoundError();
     }
-    try {
-        await AccountService.update(account, {
-            address: req.body.address,
-            googleAccess: req.body.googleAccess,
-            twitterAccess: req.body.twitterAccess,
-            spotifyAccess: req.body.spotifyAccess,
-            authenticationToken: req.body.authenticationToken,
-            authenticationTokenExpires: req.body.authenticationTokenExpires,
-        });
-        res.status(204).end();
-    } catch (error) {
-        if (error.code === 11000) {
-            throw new UnprocessableEntityError('A user for this e-mail already exists.');
-        }
-        throw error();
-    }
+    await AccountService.update(account, {
+        address: req.body.address,
+        googleAccess: req.body.googleAccess,
+        twitterAccess: req.body.twitterAccess,
+        spotifyAccess: req.body.spotifyAccess,
+        authenticationToken: req.body.authenticationToken,
+        authenticationTokenExpires: req.body.authenticationTokenExpires,
+    });
+    res.status(204).end();
 };

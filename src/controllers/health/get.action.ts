22459d13b9330b099c5a9ef51ff179cd72ff9676
Bram Rongen <bram@thx.network>
2022-02-28 09:58:55 +0100

    Remove error thrown on health

--- src/controllers/health/get.action.ts
+++ src/controllers/health/get.action.ts
@@ -2,7 +2,6 @@ import { Response, Request } from 'express';
 import { name, version, license } from '../../../package.json';
 
 export const getHealth = (_req: Request, res: Response) => {
-    throw new Error('dude');
     const jsonData = {
         name,
         version,

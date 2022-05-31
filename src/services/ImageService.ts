4c93a8308fff95ed233181dd03499040e9e37e1c
GarfDev <garfdev.13@gmail.com>
2022-05-31 20:50:41 +0700

    feat: add ACL upload'

--- src/services/ImageService.ts
+++ src/services/ImageService.ts
@@ -3,7 +3,7 @@ import { GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 import { s3Client } from '../util/s3';
-import { AWS_BUCKET_NAME } from '../util/secrets';
+import { AWS_BUCKET_NAME, AWS_BUCKET_REGION } from '../util/secrets';
 
 export default {
     upload: async (file: Express.Multer.File) => {
@@ -14,14 +14,19 @@ export default {
         const uploadParams = {
             Key: filename,
             Bucket: AWS_BUCKET_NAME,
+            ACL: 'public-read',
             Body: stream,
         };
 
         return { ...(await s3Client.send(new PutObjectCommand(uploadParams))), key: filename };
     },
-    getSignedUrl: async (key: string) => {
+    getSignedUrl: async (key: string, expiresIn = 3600) => {
         const command = new GetObjectCommand({ Bucket: AWS_BUCKET_NAME, Key: key });
-        const url = await getSignedUrl(s3Client, command, { expiresIn: 3600 }); // expires in seconds
+        const url = await getSignedUrl(s3Client, command, { expiresIn }); // expires in seconds
         return url;
+    },
+
+    getPublicUrl: (key: string) => {
+        return `https://${AWS_BUCKET_NAME}.${AWS_BUCKET_REGION}.amazonaws.com/${key}`;
     },
 };

8fd3c24bf59750284c07addda7ac3998eac84224
Peter Polman <peter@peterpolman.nl>
2022-07-08 16:02:51 +0200

    Fix certs and jwks

--- scripts/generateKeys.js
+++ scripts/generateKeys.js
@@ -1,25 +1,11 @@
-// const fs = require('fs');
-// const path = require('path');
-// const jose = require('jose');
-// const keystore = new jose.JWKS.KeyStore();
-
-// Promise.all([
-//     keystore.generate('RSA', 2048, { use: 'sig', alg: 'RS256' }),
-//     keystore.generate('RSA', 2048, { use: 'enc', alg: 'RS256' }),
-// ]).then(() => {
-//     fs.writeFileSync(path.resolve('src/jwks.json'), JSON.stringify(keystore.toJWKS(true), null, 2));
-// });
-
 const fs = require('fs');
 const path = require('path');
 const jose = require('jose');
-
 const keystore = new jose.JWKS.KeyStore();
 
 Promise.all([
-    keystore.generate('RSA', 2048, { use: 'sig' }),
-    keystore.generate('EC', 'P-256', { use: 'sig', alg: 'ES256' }),
-    keystore.generate('OKP', 'Ed25519', { use: 'sig', alg: 'EdDSA' }),
+    keystore.generate('RSA', 2048, { use: 'sig', alg: 'RS256' }),
+    keystore.generate('RSA', 2048, { use: 'enc', alg: 'RS256' }),
 ]).then(() => {
     fs.writeFileSync(path.resolve('src/jwks.json'), JSON.stringify(keystore.toJWKS(true), null, 2));
 });

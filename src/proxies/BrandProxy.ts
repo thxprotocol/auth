25114a3129cdcc82d8975ec0a11b31461cafeda5
Peter Polman <peter@peterpolman.nl>
2022-07-22 16:39:27 +0200

    Fix brandproxy

--- src/proxies/BrandProxy.ts
+++ src/proxies/BrandProxy.ts
@@ -1,25 +1,13 @@
 import { apiClient } from '../util/api';
 
-interface brandUpdateParams {
-    logoImgUrl: string;
-    backgroundImgUrl: string;
-}
-
 export default {
     get: async (poolId: string) => {
         const r = await apiClient({
             method: 'GET',
-            url: `/v1/brand/${poolId}`,
-        });
-
-        return r.data;
-    },
-
-    update: async (poolId: string, params: Partial<brandUpdateParams>) => {
-        const r = await apiClient({
-            method: 'PUT',
-            url: `/v1/brand/${poolId}`,
-            data: params,
+            url: '/v1/brands',
+            headers: {
+                'X-PoolId': poolId,
+            },
         });
 
         return r.data;

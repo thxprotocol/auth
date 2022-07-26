153165fdc76041ef94f3571a48e8d20cfef1ace3
Peter <peter@thx.network>
2022-07-26 12:14:23 +0200

    Fix metamask login page if no metamask is available

--- src/public/js/signin.js
+++ src/public/js/signin.js
@@ -1,36 +1,37 @@
 import { createApp } from 'https://unpkg.com/petite-vue?module';
 
 /* eslint-disable no-undef */
-if (typeof window.ethereum !== 'undefined') {
-    const AUTH_REQUEST_MESSAGE = document.getElementsByName('authRequestMessage')[0].value;
-    const ERROR_CONNECT_METAMASK = 'Please connect to MetaMask.';
+const AUTH_REQUEST_MESSAGE = document.getElementsByName('authRequestMessage')[0].value;
+const ERROR_CONNECT_METAMASK = 'Please connect to MetaMask.';
+const ERROR_INSTALL_METAMASK = 'Please install MetaMask.';
 
-    createApp({
-        alert: {
-            variant: 'warning',
-            message: '',
-        },
-        onAccountsChanged(accounts) {
-            if (!accounts.length) {
-                this.alert.message = ERROR_CONNECT_METAMASK;
-            } else {
-                window.ethereum
-                    .request({
-                        method: 'eth_signTypedData_v3',
-                        params: [accounts[0], AUTH_REQUEST_MESSAGE],
-                    })
-                    .then((signature) => {
-                        document.getElementsByName('authRequestSignature')[0].value = signature;
-                        document.getElementById('form-signin').submit();
-                    })
-                    .catch((err) => {
-                        if (err.code === 4001) {
-                            this.alert.message = ERROR_CONNECT_METAMASK;
-                        }
-                    });
-            }
-        },
-        signin() {
+createApp({
+    alert: {
+        variant: 'warning',
+        message: '',
+    },
+    onAccountsChanged(accounts) {
+        if (!accounts.length) {
+            this.alert.message = ERROR_CONNECT_METAMASK;
+        } else {
+            window.ethereum
+                .request({
+                    method: 'eth_signTypedData_v3',
+                    params: [accounts[0], AUTH_REQUEST_MESSAGE],
+                })
+                .then((signature) => {
+                    document.getElementsByName('authRequestSignature')[0].value = signature;
+                    document.getElementById('form-signin').submit();
+                })
+                .catch((err) => {
+                    if (err.code === 4001) {
+                        this.alert.message = ERROR_CONNECT_METAMASK;
+                    }
+                });
+        }
+    },
+    signin() {
+        if (typeof window.ethereum !== 'undefined') {
             window.ethereum
                 .request({ method: 'eth_requestAccounts' })
                 .then(this.onAccountsChanged)
@@ -41,9 +42,11 @@ if (typeof window.ethereum !== 'undefined') {
                         console.error(err);
                     }
                 });
-        },
-    }).mount();
-} else {
-    console.log('Please install MetaMask!');
-}
+        } else {
+            this.alert.message = ERROR_INSTALL_METAMASK;
+            console.info(ERROR_INSTALL_METAMASK);
+        }
+    },
+}).mount();
+
 /* eslint-enable no-undef */
